bdc004ad3ca000bd8eba6e13035f0e44da8ac6dd
Addin Munawwar <91366027+moonawar@users.noreply.github.com>
2023-12-28 01:37:27 +0700

    feat: adding navbar (unoptimized)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		fontFamily: {
+			sans: ['Space Grotesk', 'Roboto'],
+		},
+		extend: {
+			colors: {
+				primary: '#81D8DE',
+				secondary: '#060928',
+				secondary_dark: "#04061B"
+			}
+		},
 	},
 	plugins: [],
 }
