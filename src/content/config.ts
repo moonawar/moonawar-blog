1f8a70339af62488bfd721db2ab2458554e58376
Addin Munawwar <91366027+moonawar@users.noreply.github.com>
2023-12-31 02:20:29 +0700

    fix: undo react integration, we usin svelte (⌐■_■)

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,8 @@
 // Import utilities from `astro:content`
 import { z, defineCollection } from "astro:content";
 
+
+
 // Define a `type` and `schema` for each collection
 const postsCollection = defineCollection({
     type: 'content',
@@ -11,8 +13,7 @@ const postsCollection = defineCollection({
         path: z.string(),
         alt: z.string()
       }),
-      tags: z.array(z.string()),
-      keywords: z.array(z.string()).optional()  
+      tags: z.array(z.string())
     })
 });
 
