eb3d244a5489ebbc8c64ff6ae728b65e6b70a7d5
Addin Munawwar <91366027+moonawar@users.noreply.github.com>
2024-01-02 18:30:49 +0700

    feat: add breadcrumb

--- src/utils/mainUtils.ts
+++ src/utils/mainUtils.ts
@@ -5,3 +5,40 @@ export function toTagLink (tag : string) : string {
 export function getURLByPostTag(tag : string) : string {
     return `/posts/${toTagLink(tag)}/1`
 }
+
+export function toTitle(str: string) : string {
+    return str.replace(
+        /\w\S*/g,
+        function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    ).replaceAll('-', ' ');
+}
+
+interface Link {
+   href: string;
+   title: string;
+}
+
+type Links = Link[];
+
+export function extractPathNameToLinks(pathName : string) : Links {
+    const segments : string[] = pathName.split('/');
+    segments.shift(); // we don't want the element before the first '/'
+    const links : Link[] = [];
+    
+    segments.reduce((current, segment) => {
+        const href = `${current}/${segment}`
+        const title = toTitle(segment);
+
+        const link : Link = {
+            href: href,
+            title: title,
+        }
+        
+        links.push(link);
+        return href;
+    }, ''); 
+
+    return [{href:'/', title:'Home'}, ...links];
+} 
