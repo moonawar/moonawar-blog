97d29b535917fa9179a50b63310b5506a0e8584e
Addin Munawwar <91366027+moonawar@users.noreply.github.com>
2024-01-03 23:00:37 +0700

    chore: migrate navbar to svelte for some animation

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,11 +1,10 @@
----
-import { Image } from 'astro:assets';
-import nameLogo from '../assets/nameLogo.png'
----
+<script>
+    
+</script>
 
 <nav class="py-5 flex justify-center  bg-secondary_dark text-white">
     <div class="w-4/5 flex justify-between">
-        <a href="/"><Image src={nameLogo} alt="Name Logo"/></a>
+        <a href="/"><img src='/nameLogo.png' alt="Name Logo"/></a>
         <svg 
             width="34" height="27" viewBox="0 0 34 27" fill="none" 
             xmlns="http://www.w3.org/2000/svg" class="mt-2"
