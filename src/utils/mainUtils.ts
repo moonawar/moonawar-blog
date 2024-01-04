50e5eb40a03dc80d68868fdf6aeee27a08854daf
Addin Munawwar <91366027+moonawar@users.noreply.github.com>
2024-01-04 07:01:02 +0700

    fix: image size and aspect ratio error and cleaning console log

--- src/utils/mainUtils.ts
+++ src/utils/mainUtils.ts
@@ -10,7 +10,7 @@ export function toTitle(str: string) : string {
     return str.replace(
         /\w\S*/g,
         function(txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        return txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase();
         }
     ).replaceAll('-', ' ');
 }
